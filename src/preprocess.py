f82deb2385f93dc954305c6fe4adffebe30e0b7e
erMamaso <miguelangel.munoztubio@gmail.com>
2025-05-16 13:16:23 +0200

    Type-data verification added

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,14 +1,10 @@
-import pandas as pd
-
 def clean_data(input_path, output_path):
     df = pd.read_csv(input_path)
-    # In case there is any NULL value, they will be filled with mean value
     df['rating'] = df['rating'].fillna(df['rating'].median())
-    # type-conversion
-    df['year'] = df['year'].astype(int)
-    df['rating'] = df['rating'].astype(float)
+    # Verificación de tipos
+    if not df['year'].dtype == 'int':
+        df['year'] = df['year'].astype(int)
+    if not df['rating'].dtype == 'float':
+        df['rating'] = df['rating'].astype(float)
     df.to_csv(output_path, index=False)
     return df
-
-if __name__ == "__main__":
-    clean_data("data/movies.csv", "data/movies_clean.csv")
